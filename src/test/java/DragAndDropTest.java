fe6a2c6ec1acc72698cd9a3d04bebfc1232c4604
dioliver29 <diana.oliver29@gmail.com>
2022-05-02 22:35:06 +0500

    added test for draganddrop result

--- src/test/java/DragAndDropTest.java
+++ src/test/java/DragAndDropTest.java
@@ -1,3 +1,4 @@
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Selenide.*;
@@ -9,6 +10,7 @@ public class DragAndDropTest {
 
         Selenide.open("https://the-internet.herokuapp.com/drag_and_drop");
         $("#column-a").dragAndDropTo("#column-b");
+        $("#column-a").shouldHave(Condition.text("B"));
 
     }
 }
